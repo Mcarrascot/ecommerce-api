b537e064442785ba2bed3bbf4ce136f732ce7243
Montserrat Carrasco <mfcarrascot@gmail.com>
2024-07-16 03:07:51 -0400

    fix: carpeta públic

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ import userRouter from './routers/user.js';
 import paymentRouter from './routers/payment.js';
 import cors from 'cors';
 import connectDB from './db/mongoose.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT || 1234
 
@@ -16,10 +21,7 @@ app.use(express.json())
 app.use(userRouter)
 app.use(paymentRouter)
 
-
-// Serve static files from the "public" directory
 app.use('/public', express.static(path.join(__dirname, '../public')));
-
 
 app.listen(port, () => {
     console.log('server listening on port ' + port)
